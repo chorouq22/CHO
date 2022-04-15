274b3010408820433ba487e2440aacdbc4f51b76
PC <PC@DESKTOP-I4BAJLV>
2022-04-15 13:31:20 +0200

    declare

--- etudiant/src/etudiant/etudiant1.java
+++ etudiant/src/etudiant/etudiant1.java
@@ -3,6 +3,9 @@ package etudiant;
 public class etudiant1 {
 	public int id ;
 	public String nom; 
-	public double note ;
+	public double note;
+	public etudiant1(int id,String nom) {
+		double note = 10; 
+}
 
 }
